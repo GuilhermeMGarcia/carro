1ccf50ad722ba8ffa60b023ff41438cd95944b6f
G.Garci4 <66149021+OCobr4@users.noreply.github.com>
2020-07-22 11:55:02 -0300

    test_certo

---
+++ funcloop.py
@@ -0,0 +1,32 @@
+from carro import Carro
+import time
+
+
+class Funcloop:
+
+
+    @staticmethod
+    def terminal():
+        print('Carregando...')
+        time.sleep(0.5)
+        print('Bem Vindo a concecionaria de carros')
+
+        print('Selecione uma das opções.')
+
+        loop = True
+        while loop:
+            op = input('Opção 1- Adicionar Carros, 2- Ver Carros, 3- Excluir, 4- Sair.:').upper()
+            if op == '1':
+                try:
+                    Carro(marca=input('Digite a Marca:'), valor=int(input('Digite o valor:')),
+                          cor=input('Digite a cor:'), ano=int(input('Digite o ano:')))
+                except ValueError:
+                    print('Opção invalida')
+            if op == '2':
+                Carro.imprimir(self=Carro.lst)
+            if op == '3':
+                Carro.excluir(self=Carro.lst)
+            if op == '4':
+                print('Saindo...')
+                time.sleep(0.5)
+                loop = False
--- main.py
+++ main.py
@@ -1,25 +1,6 @@
-import time
-from carro import Carro
+import funcloop
 
-print('Carregando...')
-time.sleep(0.5)
-print('Bem Vindo a concecionaria de carros')
+funcloop.Funcloop.terminal()
 
-print('Selecione uma das opções.')
 
-loop = True
-while loop:
-    op = input('Opção 1- Adicionar Carros, 2- Ver Carros, 3- Excluir, 4- Sair.:').upper()
-    if op == '1':
-        try:
-            Carro(marca=input('Digite a Marca:'), valor=int(input('Digite o valor:')), cor=input('Digite a cor:'), ano=int(input('Digite o ano:')))
-        except ValueError:
-            print('Opção invalida')
-    if op == '2':
-        Carro.imprimir(self=Carro.lst)
-    if op == '3':
-        Carro.excluir(self=Carro.lst)
-    if op == '4':
-        print('Saindo...')
-        time.sleep(0.5)
-        loop = False
+
